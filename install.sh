88957450408ab0d6f9af92e9797f722374d836ec
HotInfillpattern <139463388+SHID1214@users.noreply.github.com>
2024-03-01 04:22:24 +0900

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir ~/printer_data/config/.theme
-ln -sf ~/mainsail_theme/.theme/* ~/printer_data/config/.theme/
-
-echo "@import \"custom_base.css\"" >~/printer_data/config/.theme/custom.css
